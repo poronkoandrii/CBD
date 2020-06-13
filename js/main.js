cc2ec2a1fd0ab7df1251d06f1ed8fb8752435b0f
poronkoandrii <st.parony@gmail.com>
2020-06-13 13:10:57 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ $(".slider").slick({
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 900,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
